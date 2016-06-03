da5a92975e46aa5f735aba2389ddad9bf9445d87
Torng-Lin Chen <tornglinchen@Torng-Lins-MacBook-Pro.local>
2016-06-03 15:05:15 -0400

    removed hash, reroute pages, fixed artists url

--- browser/js/module.js
+++ browser/js/module.js
@@ -7,3 +7,14 @@ juke.run(function ($rootScope) {
         console.error('Error transitioning from "' + fromState.name + '" to "' + toState.name + '":', error);
     });
 });
+
+juke.config(function ($urlRouterProvider, $locationProvider) {
+    // remove '#' from urls
+    $locationProvider.html5Mode(true);
+    // invalid routes redirect to the root
+    $urlRouterProvider.otherwise('/');
+    // reroute home page to albums
+    $urlRouterProvider.when('/', '/albums');
+    // reroute artist page to artist's ablums page
+    $urlRouterProvider.when("/artists/:artistId", "/artists/:artistId/albums");
+});
